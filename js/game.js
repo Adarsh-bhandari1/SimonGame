c4f992796c7138900165b4b3e66fdaab3c637ea3
Adarsh Bhandari <adarshsinghbhandari@gmail.com>
2025-09-13 12:13:03 +0530

    adding game pattern  generator code

---
+++ js/game.js
@@ -0,0 +1,6 @@
+// Game Pattern Generator
+var color = ["red", "brown", "green", "purple"];
+var ranNum = Math.floor(Math.random() * 3);
+var gamePattern = [];
+gamePattern.push(color[ranNum]);
+console.log(gamePattern);
